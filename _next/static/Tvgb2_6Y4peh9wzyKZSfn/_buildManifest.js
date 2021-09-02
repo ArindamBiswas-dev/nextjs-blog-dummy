895d32a18cbef6606436d77a73e1e3d36b832576
ArindamBiswas-dev <69881224+ArindamBiswas-dev@users.noreply.github.com>
2021-09-02 16:51:16 +0000

    Deploying to gh-pages from @ ArindamBiswas-dev/nextjs-blog-dummy@8be106ddaf3fd0ab38a261102a0e68f6a77473b9 🚀

--- _next/static/Tvgb2_6Y4peh9wzyKZSfn/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-048c00de701ed3b15b89.js"],"/_error":["static/chunks/pages/_error-eb7f79f2af83e8ddf5e2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/pgSZsArx1jYaXijcNqIuY/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-376f907d575fd0e299c0.js"],"/_error":["static/chunks/pages/_error-eb7f79f2af83e8ddf5e2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
